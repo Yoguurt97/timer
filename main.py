feb5e3ff47bdee4d158332797a503d3663c1ffec
Yoguurt97 <yoguurt97@mail.com>
2016-11-18 21:11:33 +1000

    asd

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def start():
     root.state('iconic')
 def stop():
     system('shutdown -a')
-
-root= Tk()
-root.geometry('200x200')
-root.title('Timer')
-var = DoubleVar()
-label = Label(root, text='Укажите время в минутах.').pack()
-scale = Scale(root, length=300, orient=HORIZONTAL, from_=1, to=120, resolution=1, variable=var).pack()
-Button(root, text='Установить время', command=start).pack()
-Button(root, text='Остановить таймер', command=stop).pack()
-root.mainloop()
+if __name__ == "__main__":
+    root= Tk()
+    root.geometry('200x200')
+    root.title('Timer')
+    var = DoubleVar()
+    label = Label(root, text='Укажите время в минутах.').pack()
+    scale = Scale(root, length=300, orient=HORIZONTAL, from_=1, to=120, resolution=1, variable=var).pack()
+    Button(root, text='Установить время', command=start).pack()
+    Button(root, text='Остановить таймер', command=stop).pack()
+    root.mainloop()
